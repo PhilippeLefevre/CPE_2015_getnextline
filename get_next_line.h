8e094dccd69c5462ea15ca8ba3047573519d6eb9
Lefevre Philippe <PhilippeLefevre@users.noreply.github.com>
2016-05-05 02:43:29 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,3 @@
-/*
-** get_next_line.h for getnextline in /home/lefevr_h/Repository/Epitech/First_1/Elementary_Programming/CPE_2015_getnextline
-** 
-** Made by Philippe Lefevre
-** Login   <lefevr_h@epitech.net>
-** 
-** Started on  Thu Mar  3 02:40:23 2016 Philippe Lefevre
-** Last update Thu Mar  3 02:40:23 2016 Philippe Lefevre
-*/
-
 #ifndef			GET_NEXT_LINE_H_
 # define		GET_NEXT_LINE_H_
 # include		<unistd.h>
